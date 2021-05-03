d7c81db66cf00fa942097c8d392c41099d09ea2d
jake666777666 <34963670+jake666777666@users.noreply.github.com>
2021-05-03 13:34:08 -0500

    Delete test1.bat

--- test1.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""C:\Users\DerekVanderver\Desktop\batch\scripts\pss.ps1""' -Verb RunAs}"
-pause
