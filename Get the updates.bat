15320f15c14dae105a15a72957035ad117980afa
aaaaaaaaaaa <34963670+DerekBoiScripts@users.noreply.github.com>
2021-10-20 14:33:09 -0500

    Add files via upload

--- Get the updates.bat
+++ Get the updates.bat
@@ -7,9 +7,19 @@ echo.
 echo File Updated OR Downloaded
 echo.
 echo extracting the zipped file
-powershell -command "Expand-Archive -Force '%~dp0RunThisMan.zip' '%~dp0'"
+powershell -command "Expand-Archive -Force '%~dp0RunThisMan.zip' 'C:\users\%USERNAME%\AppData\Local\Temp'"
+
 reg add HKEY_CLASSES_ROOT\Directory\Background\shell\RunThisMan /f >nul 2>&1
 reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\RunThisMan\command" /ve /d "C:\users\%USERNAME%\AppData\Local\Temp\RunThisMan.bat" /f >nul 2>&1
-timeout 10
+
+set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
+echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
+echo sLinkFile = "%~dp0RunThisMan.lnk" >> %SCRIPT%
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
+echo oLink.TargetPath = "C:\Users\%username%\AppData\Local\Temp\RunThisMan.bat" >> %SCRIPT%
+echo oLink.Save >> %SCRIPT%
+cscript /nologo %SCRIPT%
+del %SCRIPT%
+timeout /t 5
 
 
