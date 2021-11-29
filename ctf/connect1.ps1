5581fa9163447252441fc2898124a7d1a61de229
aaaaaaaaaaa <34963670+DerekBoiScripts@users.noreply.github.com>
2021-11-29 09:38:53 -0600

    Delete ctf directory

--- ctf/connect1.ps1
+++
@@ -1,7 +0,0 @@
-cmdkey /list | ForEach-Object{if($_ -like "*target=TERMSRV/*"){cmdkey /del:($_ -replace " ","" -replace "Target:","")}}
-echo "Connecting to "
-$Server="ip"
-$User="admin"
-$Password="1234"
-cmdkey /generic:TERMSRV/$Server /user:$User /pass:$Password
-mstsc /v:$Server
