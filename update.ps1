3455021d9f15b0aa2109c874c12f7cc223aac0d1
aaaaaaaaaaa <34963670+DerekBoiScripts@users.noreply.github.com>
2022-02-10 07:58:37 -0600

    Add files via upload

--- update.ps1
+++ update.ps1
@@ -1,3 +1,14 @@
-﻿(New-Object System.Net.WebClient).Downloadstring('https://raw.githubusercontent.com/DerekBoiScripts/testbatch/main/improvedGUI.ps1')
-
+﻿$ls = (New-Object System.Net.WebClient).Downloadstring('https://raw.githubusercontent.com/DerekBoiScripts/testbatch/main/improvedGUI.ps1')
+try 
+{
+    Write-Output "[*] Updating improvedGUI.ps1"
+    Remove-Item "$($PSScriptRoot)\improvedGUI.ps1"
+    $ls | Out-File "$($PSScriptRoot)\improvedGUI.ps1"
+}
+catch [System.Exception] {
+    Write-Output "Error saving new version of improvedGUI.ps1"
+    throw
+	Read-Host "Press any key to exit."
+    exit
+}
 pause
